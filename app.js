491dc4678ccc712dcbfab32fbda24c93a4e4a602
deivan <ariastation@ua.fm>
2017-07-19 19:53:29 +0300

    added end of game

--- app.js
+++ app.js
@@ -66,17 +66,36 @@ class CrazyRace {
     }
     
     function getStep () {
+      let flag = null, endOfLine = 420;
       self.bugs.map( (bug, index) => {
         console.log(bug.id, bug.style.left, parseFloat(bug.style.left), speeds[index]);
         let x = parseFloat(bug.style.left);
         x += speeds[index];
         bug.style.left = x + 'px';
+        if (x > endOfLine) flag = index +1;
       });
-//      setTimeout(()=> {
-//        getStep();
-//      }, 1000);
+      if (!!flag) {
+        self.showResult(flag);
+      } else {
+        setTimeout(()=> {
+          getStep();
+        }, 100);
+      }
+      
     }
-    
+  }
+  
+  showResult (winner) {
+    let win = document.getElementById('result-win'),
+        loo = document.getElementById('result-loose');
+    document.getElementById('results').style.display = 'block';
+    if (winner == this.selectedNumber) {
+      win.classList.remove('hidden');
+      loo.classList.add('hidden');
+    } else {
+      win.classList.add('hidden');
+      loo.classList.remove('hidden');
+    }
   }
 }
   
