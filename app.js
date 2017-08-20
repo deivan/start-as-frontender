6b15d52b898385fa6a37beaf3fbb85c82505a6d0
deivan <ariastation@ua.fm>
2017-08-20 21:08:30 +0300

    started UI base

--- app.js
+++ app.js
@@ -7,42 +7,7 @@ $(document).ready( () => {
 
 class Catalog {
   constructor () {
-    this.data = null;
-    this.current = 'r61';
-    this.getData();
+    //$( "#accordion" ).accordion();
   }
-  
-  getData () {
-    $.getJSON('rename.json', res => {
-      this.data = res;
-      this.fillSelector();
-      this.fillTable();
-    });
-  }
-  
-  fillSelector () {
-    let result = '', self = this;
-    for (let key in this.data) {
-      if (typeof this.data[key] === 'object')
-        result += `<option value="${key}">${this.data[key].newAreaName}</option>`; 
-    }
-    $('#area-selector').html(result).on('change', () => {
-      this.current = $('#area-selector').val();
-      this.fillTable();
-    });
-  }
-  
-  fillTable () {
-    let streets = this.data[this.current].objects, result = '';
-    streets.map( item => {
-      let names = `<tr><td>${item.oldName}</td><td>${item.newName}</td>`,
-          link = !!item.link 
-            ? `<td><a href="${item.link.href}">link</a></td>` 
-            : `<td></td>`;
-      result += names + link + `<td></td></tr>`;
-    });
-    $('#table-data').html(result);
-  }
-  
   
 }
