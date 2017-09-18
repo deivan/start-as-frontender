c27ae79297f1284afdc01f4f706243dfac714d9d
deivan <ariastation@ua.fm>
2017-09-18 21:09:21 +0300

    results

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
 class Main {
   constructor () {
     console.log('started!');
+    Vue.use(VueMaterial);
+    this.makeApp();
   }
   
   makeApp () {
+    let self = this;
     this.app = new Vue({
       el: '#app-instance',
       data: {
-        
+        citizens: [
+          {id:1, name:'Gordon Freeman', status: 'a hero'},
+          {id:2, name:'Judith Mossman', status: 'scientist'},
+          {id:3, name:'Alix Vance', status: 'rebel'},
+          {id:4, name:'Isaac Kleiner', status: 'scientist'}
+        ],
+        newPerson: {
+          id: 0,
+          name:'',
+          status:''
+        },
+        showEdit: false
       },
       methods: {
-        
+        save: function () {
+          this.citizens.push(this.newPerson);
+          this.newPerson = {
+            id: 0,
+            name:'',
+            status:''
+          };
+          this.showEdit = false;
+        },
+        deleteRow: function (i) {
+          this.citizens = this.citizens.filter( item => item.id !== i)
+        }
       }
     });
   }
