45919286b7aefe46eecfc5c139bb17d3f684f422
deivan <ariastation@ua.fm>
2017-09-19 18:59:36 +0300

    start

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ class Main {
   constructor () {
     console.log('started!');
     Vue.use(VueMaterial);
-    this.makeApp();
+    //this.makeApp();
   }
   
   makeApp () {
@@ -10,32 +10,11 @@ class Main {
     this.app = new Vue({
       el: '#app-instance',
       data: {
-        citizens: [
-          {id:1, name:'Gordon Freeman', status: 'a hero'},
-          {id:2, name:'Judith Mossman', status: 'scientist'},
-          {id:3, name:'Alix Vance', status: 'rebel'},
-          {id:4, name:'Isaac Kleiner', status: 'scientist'}
-        ],
-        newPerson: {
-          id: 0,
-          name:'',
-          status:''
-        },
-        showEdit: false
+        data:[],
+        version: null
       },
       methods: {
-        save: function () {
-          this.citizens.push(this.newPerson);
-          this.newPerson = {
-            id: 0,
-            name:'',
-            status:''
-          };
-          this.showEdit = false;
-        },
-        deleteRow: function (i) {
-          this.citizens = this.citizens.filter( item => item.id !== i)
-        }
+
       }
     });
   }
