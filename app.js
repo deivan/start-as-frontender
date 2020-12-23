56bc4f105c7c2c8e91f4b52371545f2688433a23
NikitaSparkle <71117913+NikitaSparkle@users.noreply.github.com>
2020-12-24 00:26:11 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-$(".page1_button1").click(function(){
+$(".page1_button1").click(function() {
     $(".page1").hide();
     $(".page2").show();
 });
 
-$(".page1_button2").click(function(){
+$(".page1_button2").click(function() {
     $(".page1").hide();
     $(".page2").show();
 });
 
-$(".page2_button1").click(function(){
+$(".page2_button1").click(function() {
     $(".page2").hide();
     $(".page3").show();
 });
 
-$(".page2_button2").click(function(){
+$(".page2_button2").click(function() {
     $(".page2").hide();
     $(".page3").show();
 });
 
-$(".page3_button").click(function(){
+$(".page3_button").click(function() {
     $(".page3").hide();
     $(".page4").show();
 });
 
-$(".page2_arrow").click(function(){
+$(".page2_arrow").click(function() {
     $(".page2").hide();
     $(".page1").show();
 });
 
-$(".page3_arrow").click(function(){
+$(".page3_arrow").click(function() {
     $(".page3").hide();
     $(".page2").show();
 });
 
-$(".page4_arrow").click(function(){
+$(".page4_arrow").click(function() {
     $(".page4").hide();
     $(".page3").show();
 });
 
-$(".page4_button").click(function(){
+$(".page4_button").click(function() {
     alert("Thank you for yoir choise!");
 });
