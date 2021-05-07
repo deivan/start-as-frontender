f0905cf7ffb4288de74f752be44b193409e42a2f
Ivan <idemenkov@archer-soft.com>
2021-05-07 22:37:58 +0300

    added xhttprequest

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 window.onload = () => {
     console.log('app started.');
-    // http://rename.dp.ua/rename.json 
+    //xhttp();
+    //fetcH(); 
+};
 
-    let promise = fetch('https://api.github.com/users/deivan', {
-      method: "GET",
-      headers: {"Content-type": "application/json;charset=UTF-8"}
-    })
+function xhttp() {
+  let xhr = new XMLHttpRequest();
 
-    promise.then(res => res.json())
+  xhr.open('GET', 'https://api.github.com/users/deivan', true);
+  xhr.responseType = 'json';
+  xhr.send();
+  xhr.onload = function() {
+    console.log(`status: ${xhr.status} - ${xhr.statusText}`);
+    console.log(`response: ${JSON.stringify(xhr.response)}`);
+  };
+  
+  xhr.onerror = function() {
+    console.error('error');
+  };
+}
+
+function fetcH() {
+  // https://jsonplaceholder.typicode.com/users
+  // https://jsonplaceholder.typicode.com/photos
+  // http://rename.dp.ua/rename.json
+  let promise = fetch('https://api.github.com/users/deivan', {
+    method: "GET",
+    headers: {"Content-type": "application/json;charset=UTF-8"}
+  })
+
+  promise
+    .then(res => res.json())
     .then(json => console.log(json))
     .catch(res => {
       console.error(res);
     });
-};
+}
