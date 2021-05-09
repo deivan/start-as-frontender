ab15e457e3e6f6bc0908f51e9e54faa92378ee4c
Ivan <idemenkov@archer-soft.com>
2021-05-09 13:08:21 +0300

    finished

--- app.js
+++ app.js
@@ -6,4 +6,26 @@
 
 window.onload = () => {
     console.log('app started.', DATABASE);
+    const PLP = document.getElementById('container');
+    let template = '';
+
+    DATABASE.forEach(item => {
+      let element = new Render('template', item);
+      template += element.render();
+    });
+    PLP.innerHTML = template;
 };
+
+class Render {
+  constructor(templateId, data) {
+    this.template = document.getElementById(templateId).innerText;
+    this.data = data;
+  }
+
+  render() {
+    for(let key in this.data) {
+      this.template = this.template.replace(`{{${key}}}`, this.data[key]);
+    }
+    return this.template;
+  }
+}
