9637f1d197c3041bd2138193c8922e962d2be442
Ivan <idemenkov@archer-soft.com>
2021-05-11 21:49:58 +0300

    updated start conditions

--- app.js
+++ app.js
@@ -4,11 +4,29 @@
 *   <div class="myAccordeon">
 *       <div class="myAccordeon__item">
 *           <div class="myAccordeon__title">
-*               <span class="myAccordeon__title--text"></span>
-*               <span class="myAccordeon__title--chevron"></span>
+*               <span class="myAccordeon__title--text">First title</span>
+*               <span class="myAccordeon__title--chevron chevron-down"></span>
 *           </div>
-*           <div class="myAccordeon__content">
-*               ...
+*           <div class="myAccordeon__content hidden">
+*               Text content 1
+*           </div>
+*       </div>
+*       <div class="myAccordeon__item">
+*           <div class="myAccordeon__title">
+*               <span class="myAccordeon__title--text">Second title</span>
+*               <span class="myAccordeon__title--chevron chevron-down"></span>
+*           </div>
+*           <div class="myAccordeon__content hidden">
+*               Text content 2
+*           </div>
+*       </div>
+*       <div class="myAccordeon__item">
+*           <div class="myAccordeon__title">
+*               <span class="myAccordeon__title--text">Third title</span>
+*               <span class="myAccordeon__title--chevron chevron-down"></span>
+*           </div>
+*           <div class="myAccordeon__content hidden">
+*               Text content 3
 *           </div>
 *       </div>
 *   </div>
