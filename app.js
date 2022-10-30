cb00d86f675e7d24dd4d5135ee7205ff5b31d665
Ivan Demenkov <ariastation@ua.fm>
2022-10-30 12:05:20 +0200

    updated practice

--- app.js
+++ app.js
@@ -1,60 +1,37 @@
-window.onload = () => {
-  console.log('loaded');
-  const jv = new JustVidget({
-    id: 'app-core',
-    price: '$10',
-    image: '',
-    strings: ['efef  efefe', 'efeg thrth wefwefg', 'eergerg gerger ergergr egrgerg']
-  }),
-  a1 = new JustVidget({
-    id: 'app-core-a',
-    price: '$50',
-    image: '',
-    strings: ['edvdv fefd vdvd  efefe', 'th wef wefg', 'eer rg gerger ergergr egrgerg']
-  }),
-  a2 = new JustVidget({
-    id: 'app-core-b',
-    price: '$99',
-    image: '',
-    strings: ['efef  efefe', 'efeg thrth wefwefg', 'eergerg gerger ergergr egrgerg']
-  });
-};
+/*
+*   There is a block for long comments
+*
+*/
 
-class JustVidget {
-  constructor (config) {
-    console.log('class created');
-    this.container = document.getElementById(config.id);
-    this.strings = config.strings;
-    this.price = config.price;
-    this.image = config.image;
-    this.prepareBase();
-  }
-  
-  prepareBase () {
-    this.components = {
-      image: this.makeElement('img','','image'),
-      price: this.makeElement('span', this.price,'price'),
-      content: this.makeElement('div', '', 'content')
-    };
-    this.components.image.src = this.image;
-    this.container.appendChild(this.components.image);
-    this.container.appendChild(this.components.content);
-    this.container.appendChild(this.components.price);
-    this.prepareContent();
-  }
-  
-  prepareContent () {
-    let ul = this.makeElement('ul');
-    this.strings.map( (item, i) => {
-      ul.appendChild(this.makeElement('li', this.strings[i]));
-    });
-    this.components.content.appendChild(ul);
-  }
-  
-  makeElement (tagName, content='', className='') {
-    let newElement = document.createElement(tagName);
-    newElement.appendChild(document.createTextNode(content));
-    newElement.className = className;
-    return newElement;
-  }
+var a = 1;
+const PI = 3.14;
+let str = 'Hello JS';
+
+// One line comment
+
+console.log(`3: I'm in the separate file`);
+console.log(a, PI, str);
+
+//first();
+//second();
+//write('I love JS');
+
+//(function() { console.log(`4: I'm a immediate function`)})();
+
+function first() {
+  let a = 2;
+  console.log(a);
+}
+
+function second() {
+  console.log(a);
+  let a = 3;
+}
+
+const write = txt => {
+  let container = document.getElementById('container');
+  let textNode = document.createElement('P');
+
+  textNode.innerText = txt;
+  container.append(textNode);
 }
