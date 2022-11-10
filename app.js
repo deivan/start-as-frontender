24a60594a9be6bb1d28416076b4cabb6b85143c2
Ivan Demenkov <ariastation@ua.fm>
2022-11-10 22:25:03 +0200

    updated start game structure

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+// a base config should be there
+const CONFIG = {
+
+};
+
 window.onload = () => {
+
+};
+
+class RaceGame {
+  constructor() {
+    console.log('Go-Go-Go');
+  }
+
+  createUI() {
+    // there we should to prepare Game Lines and UI elemnts 
+    // which will be take a part into game processing
+  }
+
+  addEvents() {
+    // there we should to prepare all clicks to buttons
+  }
+
+  start() {
+    // Game will starts here
+  }
+
+  run() {
+    // an animation for Game
+  }
+
+  end() {
+    // there we will operate the end of Game
+  }
 
 };
