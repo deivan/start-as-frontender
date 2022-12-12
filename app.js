d688e9c8210a92149a08f3c0895d6fbdf96374cb
Ivan Demenkov <ariastation@ua.fm>
2022-12-12 21:43:11 +0200

    added lesson 14

---
+++ app.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+    document.querySelector('#getInfo').addEventListener('click', () => {
+
+    });
+    document.querySelector('#getUsers').addEventListener('click', () => {
+
+    });
+    document.querySelector('#getWrong').addEventListener('click', () => {
+
+    });
+    document.querySelector('#getUserDetails').addEventListener('click', () => {
+
+    });
+});
