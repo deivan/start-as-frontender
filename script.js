a5d5c807cc2b201e4179f71fd06f7163fc0b4ef2
Ivan Demenkov <ariastation@ua.fm>
2024-03-25 18:56:40 +0200

    added lesson base

---
+++ script.js
@@ -0,0 +1,30 @@
+const vector = [100, 24, 1, 36, 17, 19, 44, 56, 97, 72, 65, 33, 12];
+const matrix = [
+    [2, 1, 4, 5, 6, 7],
+    [31, 24, 64, 11, 12, 30],
+    [91, 53, 13, 84, 49, 51]
+];
+const objects = [
+    { id: 1, name: 'ABCD', status: 'thing' },
+    { id: 2, name: 'TFCE', status: 'liquid' },
+    { id: 3, name: 'UHGF', status: 'gas' },
+    { id: 4, name: 'RDDS', status: 'thing', disabled: true }
+];
+
+console.log('Our data is:', vector, matrix, objects);
+
+// for()
+
+// for..of
+
+// .forEach()
+
+// .map()
+
+// .filter()
+
+// .reduce()
+
+// .every()
+
+// .some()
