81d96a8adc87c6721ab6ce9440b18f1cd07e806a
Ivan Demenkov <ariastation@ua.fm>
2025-04-16 15:52:05 +0300

    updated fake API service

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
+const Results = document.querySelector('#results');
+const Details = document.querySelector('#details');
+const URL = 'https://jsonplaceholder.typicode.com/';
+
 window.addEventListener('load', () => {
-    document.querySelector('#getInfo').addEventListener('click', () => {
-
-    });
-    document.querySelector('#getUsers').addEventListener('click', () => {
-
-    });
-    document.querySelector('#getWrong').addEventListener('click', () => {
-
-    });
-    document.querySelector('#getUserDetails').addEventListener('click', () => {
-
-    });
+  document
+    .querySelector('#getUsers')
+    .addEventListener('click', getUsersData);
+  document
+    .querySelector('#getUser')
+    .addEventListener('click', getUserData);
+  document
+    .querySelector('#getGetAlbums')
+    .addEventListener('click', getAlbumsData);
+  document
+    .querySelector('#getPhotos')
+    .addEventListener('click', getPhotosData);
 });
+
+function getUsersData() {
+
+}
+
+function getUserData() {
+
+}
+
+function getAlbumsData() {
+
+}
+
+function getPhotosData() {
+
+}
